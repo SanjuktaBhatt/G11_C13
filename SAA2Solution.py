96fbd7fda9abd2bbf70464e78eac2358c7dd8a87
SanjuktaBhatt <64887786+SanjuktaBhatt@users.noreply.github.com>
2021-08-26 08:00:06 +0530

    Create SAA2Solution.py

---
+++ SAA2Solution.py
@@ -0,0 +1,10 @@
+def calc(a,b,operation):
+  if operation=="add":
+    return a+b
+  if operation=="subtract":
+    return a-b
+  if operation=="multiply":
+    return a*b
+  if operation=="divide":
+    return a/b
+calc(4,5,"multiply")
